3a5c04d72fcd356a1faac629465e9af5be401e9e
TurtleSnakeMoose <misharoskin@gmail.com>
2021-01-30 20:43:07 +0200

    deletion for new upload

--- src/components/ImportExport.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ImportExport = () => {
-    return (
-        <div>ImportExport</div>
-    );
-}
-
-export default ImportExport;
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import {reducer as formReducers} from 'redux-form';
-
-import {participantReducer} from './participantReducer'
-
-export default combineReducers({
-    participants: participantReducer,
-    form: formReducers
-});
